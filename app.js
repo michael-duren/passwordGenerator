b8c4124756556b0657b2d1ba507a6a19f466f8eb
Michael Duren <100647362+michaeldit@users.noreply.github.com>
2022-09-10 08:36:02 -0500

    Add files via upload
    Added application files

---
+++ app.js
@@ -0,0 +1,61 @@
+let specialChar = '!';
+
+const curChar = document.querySelector('#currentChar');
+
+const btnEx = document.querySelector('#spEx');
+btnEx.addEventListener('click', () => {
+  specialChar = '!';
+  curChar.innerText = specialChar;
+});
+const btnAt = document.querySelector('#spAt');
+btnAt.addEventListener('click', () => {
+  specialChar = '@';
+  curChar.innerText = specialChar;
+});
+const btnPo = document.querySelector('#spPo');
+btnPo.addEventListener('click', () => {
+  specialChar = '#';
+  curChar.innerText = specialChar;
+});
+const btnDo = document.querySelector('#spDo');
+btnDo.addEventListener('click', () => {
+  specialChar = '$';
+  curChar.innerText = specialChar;
+});
+const btnPe = document.querySelector('#spPe');
+btnPe.addEventListener('click', () => {
+  specialChar = '%';
+  curChar.innerText = specialChar;
+});
+const btnAm = document.querySelector('#spAm');
+btnAm.addEventListener('click', () => {
+  specialChar = '&';
+  curChar.innerText = specialChar;
+});
+
+// Get password length
+const getLength = document.querySelector('#passwordLength');
+//Display special charcter
+
+// Generate button
+const generateButton = document.querySelector('#generate');
+generateButton.addEventListener('click', () => {
+  const passwordLength = getLength.value;
+  var randomPassword = generatePassword(passwordLength, specialChar);
+  const phonetics = generatePhonetics(randomPassword, specialChar);
+  const passwordOutput = document.querySelector('#passwordOutput');
+  passwordOutput.innerText = randomPassword;
+  const phoneticsOutput = document.querySelector('#passwordSpelling');
+  phoneticsOutput.innerText = phonetics;
+});
+
+// Copy paste button
+const copyClipboardButton = document.querySelector('#copyClipboard');
+copyClipboardButton.addEventListener('click', async () => {
+  const password = document.querySelector('#passwordOutput');
+  const copyText = password.innerText;
+  const copyNotification = document.querySelector('#copyNotification');
+
+  await navigator.clipboard.writeText(copyText);
+  copyNotification.innerText = `Copied ${copyText}`;
+});
---
+++ helper.js
@@ -0,0 +1,37 @@
+const generateRandomLetter = () => {
+  return Math.floor(Math.random() * 26);
+};
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const generatePassword = (passwordLength, specialChar) => {
+  let password = '';
+  password += upperCase[generateRandomLetter()];
+  for (let i = 0; i < passwordLength - 6; i++) {
+    password += lowerCase[generateRandomLetter()];
+  }
+  for (let i = 0; i < 4; i++) {
+    password += numbers[generateRandomNumber()];
+  }
+  password += specialChar;
+
+  return password;
+};
+
+const generatePhonetics = (password, specialChar) => {
+  let milPhonetics = 'Capital ';
+  for (const key in milAlph) {
+    if (password[0] === key) {
+      milPhonetics += `${milAlph[key]},`;
+    }
+  }
+  for (let i = 1; i < password.length; i++) {
+    for (let key in milAlph) {
+      if (password[i].toUpperCase() === key) {
+        milPhonetics += ` ${milAlph[key].toLowerCase()},`;
+      }
+    }
+  }
+  return `${milPhonetics} ${specialChar}`;
+};
